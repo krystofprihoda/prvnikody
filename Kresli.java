cd4ee486027ed7597f0bf347300cc5d5b36e93ad
Krystof Prihoda <prihoda46@gmail.com>
2017-01-12 16:10:13 +0100

    1212017

---
+++ Kresli.java
@@ -0,0 +1,32 @@
+public class Kresli {
+  public static void main(Strings[] args) {
+  
+    int indexTvar = 0;
+    int indexArgs = 0;
+    
+    if (args.length == 0){
+      System.out.printf("Zadejte cisla.");
+      break;
+    }
+    
+    while (indexTvar <= args.length){
+      //bod
+      if (Integer.valueOfindexTvar == 0) {
+        
+        index = index + 3;
+      }
+    
+      //kruh
+      if (cislo == 1) {
+        
+        index = index + 4;
+      }
+    
+      //obdelnik
+      if (cislo == 2) {
+        
+        index = index + 5;
+      }
+    }
+  } 
+}
