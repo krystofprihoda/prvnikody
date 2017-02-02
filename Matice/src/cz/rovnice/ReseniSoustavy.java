b17a7103e2b0b0956bbf75973062993e3dec1cf0
Krystof Prihoda <prihoda46@gmail.com>
2017-02-02 15:58:12 +0100

    Zacatek matice

---
+++ Matice/src/cz/rovnice/ReseniSoustavy.java
@@ -0,0 +1,28 @@
+package cz.rovnice;
+
+import java.util.Scanner;
+
+public class ReseniSoustavy {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x = 2;
+		int y = 3;
+		int[][] matice = new int[x][y];
+
+		while (sc.hasNextInt()) {
+			//prvni pole
+			for (int i = 0; i < 3; i++) {
+				sc.NextInt() = matice[1][i];
+			}
+			
+			//druhe pole
+			for (int i = 0; i < 3; i++) {
+				
+			}
+		}
+		sc.close();
+	}
+
+}
